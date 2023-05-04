1150d3940c3641e375975ef914b91629684541c8
brishavK71 <119672530+brishavK71@users.noreply.github.com>
2023-05-04 15:32:51 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello this is how we use codepipeline for hosting react site 
         </a>
       </header>
     </div>
